7d048de910a4de55d41c37b9f6f36931ae393dc3
abhinavSingh <abhi.anu003@gmail.com>
2020-12-06 15:12:10 +0530

    last commit

--- login.py
+++ login.py
@@ -1,6 +1,22 @@
-passphrase = []
+from os import path
 
-if len(passphrase)>0 :
-    login();
-else :
-    logon();
+
+    
+while True:
+    if(str(path.exists("d:\password.txt")) == "True"):
+        pass_input = input("Enter your password : ")
+        with open('d:\password.txt') as f:
+            first_line = f.readline()
+        if pass_input == first_line != "":
+            print("Menu")
+            break
+        else:
+            print("Incorrect Password, please try again.")
+            
+
+    else:
+        f = open("d:\password.txt","w+")
+        print("No password detected ,set a password for future reference.")
+        password = input("Enter a new Password : ")
+        with open('d:\password.txt', 'w') as f:
+            f.write("%s" % password)
