2b261e65d0b75e8acf93ac2baa71ee12a3099af0
abhinavSingh <abhi.anu003@gmail.com>
2020-12-07 23:36:22 +0530

    sha1

--- hasher and unhasher.py
+++ hasher and unhasher.py
@@ -1,5 +1,19 @@
-import hashlib
-password = input("Enter the password : ")
-a = hashlib.md5(password.encode())
+# README
+#
+# For md5 hashing
+#a = hashlib.md5(salted_password.encode())
 
-print(a)
+import hashlib
+
+
+password = input("Enter the password : ")
+salt = "RKh?^rLYSBf#nD-2tGzjx^zXy+q#Ph=^kb^r6&A_9NAhdh7r7k%H!d%-k%5D@C5-ysn=dd-rwKMR6#y_yR@Ds^#4-Wc-hy3&aKT&"
+
+salted_password = password + salt
+
+a = hashlib.sha1(b'Hello World')
+hex_dig = a.hexdigest()
+
+
+print(hex_dig)
+
