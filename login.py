ccb8937780f96c39fa16b5ab649fa3468f1b54cb
abhinavSingh <abhi.anu003@gmail.com>
2020-12-07 23:05:33 +0530

    logic OP

--- login.py
+++ login.py
@@ -1,20 +1,30 @@
-from os import path
+from os import path 
+import hashlib
+id = 0
+def passset(id):
+    f = open("d:\password.txt","w+")
+    if id == 1:
+        print("Reset Password initiated ..") 
+    else:
+        print("No password detected ,set a password for future reference.")
+    password = input("Enter a new Password : ")
+    with open('d:\password.txt', 'w') as f:
+        f.write("%s" % password)
 
 while True:
     if(str(path.exists("d:\password.txt")) == "True"):
         pass_input = input("Enter your password : ")
-        with open('d:\password.txt') as f:
+        with open('d:\password.txt') as f :
             first_line = f.readline()
         if pass_input == first_line != "":
-            print("Menu")
+            print("Access Granted.")
             break
+        elif pass_input == "resetpassword" :
+            id = 1
+            passset(id)
         else:
             print("Incorrect Password, please try again.")
             
-
     else:
-        f = open("d:\password.txt","w+")
-        print("No password detected ,set a password for future reference.")
-        password = input("Enter a new Password : ")
-        with open('d:\password.txt', 'w') as f:
-            f.write("%s" % password)
+        id = 0
+        passset(id)
