cfdca89da9ee4792ca9be72ac3c870f7f1f88acc
abhinavSingh <abhi.anu003@gmail.com>
2020-12-08 00:24:16 +0530

    testing_phase-2

--- login.py
+++ login.py
@@ -5,6 +5,14 @@ import hashlib
 id = 0
 salt = "RKh?^rLYSBf#nD-2tGzjx^zXy+q#Ph=^kb^r6&A_9NAhdh7r7k%H!d%-k%5D@C5-ysn=dd-rwKMR6#y_yR@Ds^#4-Wc-hy3&aKT&"
 
+def hashing(key):
+    salted = salt + key
+    temp = bytes(salted, 'utf-8') 
+    obj = hashlib.sha1(temp)
+    hex_dig = obj.hexdigest()
+    return hex_dig
+
+
 def passset(id):
     f = open("d:\password.txt","w+")
 
@@ -16,23 +24,24 @@ def passset(id):
 
     
     password = input("Enter a new Password : ")
-
+    
     with open('d:\password.txt', 'w') as f:
-        f.write("%s" % password)
+        f.write("%s" % hashing(password))
 
 while True:
     if(str(path.exists("d:\password.txt")) == "True"):
         pass_input = input("Enter your password : ")
 
         #unhashing is performed here
-        
+        a = hashing(pass_input)
 
         with open('d:\password.txt') as f :
             first_line = f.readline()
+        b = hashing(first_line)
 
 
 
-        if pass_input == first_line != "":
+        if a == b != "":
             print("Access Granted.")
             break
         elif pass_input == "resetpassword" :
